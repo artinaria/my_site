9cc40e2ec746ae6e54cc4bbe9bf0c3bee15fa958
sahar <taherpour.saha@gmail.com>
2023-01-02 14:55:28 +0330

    create template
    create main template

--- website/views.py
+++ website/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1> index_view </h1>')
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1> about_view </h1>')
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1> contact_view </h1>')
-
+    return render(request,'website/contact.html')
