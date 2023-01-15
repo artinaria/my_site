36fa8cafa5ce63834be2a2b11b8d20e9116c2d12
sahar <taherpour.saha@gmail.com>
2023-01-15 23:03:22 +0330

    connect data with page in blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    empty_value_display = '-empty-'
+    list_display=('title','status','created_date','published_date')
+    list_filter=('status',)
+    search_fields=['title','content']
+
+admin.site.register(Post,PostAdmin)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from website.models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    list_display=('name','email','created_date')
+    list_filter=('email',)
+    search_fields=['name','subject','message']
+    
+
+admin.site.register(Contact,ContactAdmin)    
+
